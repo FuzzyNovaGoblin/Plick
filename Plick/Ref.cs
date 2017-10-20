2d55c07fa4aa958045c626ff432a131c1c63956c
Grant Hedley <fuzzy.nova.goblin@outlook.com>
2017-10-19 21:55:20 -0400

    Add project files.

---
+++ Plick/Ref.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Graphics;
+using System.Xml.Linq;
+using Org.Json;
+using Newtonsoft.Json;
+using System.IO;
+using Android.Util;
+
+namespace Plick
+{
+    class Ref
+    {
+        public static Dictionary<int, Texture2D> tileTypes;
+        public static List<Tile> tileList = new List<Tile>();
+
+        public static void CreateTypes(Context context)
+        {
+            string response;
+
+            StreamReader strm = new StreamReader(context.Assets.Open("map.json"));
+            response = strm.ReadToEnd();
+
+            JSONObject jSON = new JSONObject(response);
+            Log.Debug("json", jSON.ToString());
+        }
+    }
+}
---
+++ Plick/Tile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Plick
+{
+    class Tile
+    {
+        Vector2 pos;
+        Texture2D tex;
+        Rectangle sourceRect;
+        public Tile(Vector2 pos, Texture2D tex, Rectangle sourceRect)
+        {
+            this.pos = pos;
+            this.tex = tex;
+            this.sourceRect = sourceRect;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture: tex,position: pos, sourceRectangle:sourceRect);
+        }
+    }
+}
